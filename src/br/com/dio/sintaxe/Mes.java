43f4f65032de76a377fc052632152879c617c938
MendesPombal <mendespombal2@gmail.com>
2023-01-11 17:27:02 -0300

    foram adicionados as seguintes exemplos: Operadoreslogicos e relacionais

---
+++ src/br/com/dio/sintaxe/Mes.java
@@ -0,0 +1,59 @@
+package br.com.dio.sintaxe;
+
+public class Mes {
+    public static void main(String[] args) {
+        int mes = 9;
+        if(mes == 1){
+            System.out.println("janeiro");
+        }else {
+            if(mes == 2){
+                System.out.println("fevereiro");
+            }else{
+                if(mes == 3){
+                    System.out.println("março");
+                }else{
+                    if(mes == 4){
+                        System.out.println("abril");
+                    }else{
+                        if(mes ==5){
+                            System.out.println("maio");
+                        }else{
+                            if(mes==6){
+                                System.out.println("junho");
+                            }else{
+                                if(mes==7){
+                                    System.out.println("julho");
+                                }else{
+                                    if(mes==8){
+                                        System.out.println("agosto");
+                                    }else{
+                                        if(mes==9){
+                                            System.out.println("setembro");
+                                        }else{
+                                            if(mes==10){
+                                                System.out.println("outubro");
+                                            }else{
+                                                if(mes==11){
+                                                    System.out.println("novembro");
+                                                }else{
+                                                    if(mes==12){
+                                                        System.out.println("dezembro");
+                                                    }else{
+                                                        System.out.println("Informação Inválida");
+                                                    }
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+        }
+
+    }
+
+
+}
